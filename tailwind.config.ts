32a87b185ffacbc2e39f5baca585d90c142672f6
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2024-02-10 20:56:42 +0500

    Update of Page.tsx file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lobster: ['"Lobster"', 'cursive'], // Add this line
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
